3ace4cddeb7765061fc0d1a65f82b7f05425e113
Vanessa Silva de Souza <vanessa.s.souz@gmail.com>
2023-07-24 16:55:24 -0300

    Testando alterar e depois retornar

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados ")
+console.log("Testar retornar ao projeto anterior1 ")
+console.log("Testar retornar ao projeto anterior2 ")
 
+console.log("Retorne aqui 1")
